7d77ea41ada62f3172d33c67344a4394c2dd8424
ashikul <alam.ashikul@gmail.com>
2017-07-10 18:17:25 -0400

    still not working

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,47 +1,114 @@
-import { Component } from '@angular/core';
-import { NavController, Platform } from 'ionic-angular';
-import { SpeechRecognition } from '@ionic-native/speech-recognition';
-import { ChangeDetectorRef } from '@angular/core';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {
+    SpeechRecognition,
+    SpeechRecognitionListeningOptionsAndroid,
+    SpeechRecognitionListeningOptionsIOS
+} from '@ionic-native/speech-recognition';
+import {Platform} from 'ionic-angular';
+
+import {AlertController} from 'ionic-angular';
+// import {Observable} from'rxjs/Observable';
 
 @Component({
     selector: 'page-home',
     templateUrl: 'home.html'
 })
 export class HomePage {
-    matches: String[];
-    isRecording = false;
 
-    constructor(public navCtrl: NavController, private speechRecognition: SpeechRecognition, private plt: Platform, private cd: ChangeDetectorRef) {
+
+    speechList: Array<string> = [];
+    androidOptions: SpeechRecognitionListeningOptionsAndroid;
+    iosOptions: SpeechRecognitionListeningOptionsIOS;
+
+    constructor(public navCtrl: NavController, private platform: Platform, private speech: SpeechRecognition, private alertCtrl: AlertController) {
+
     }
 
-    isIos() {
-        return this.plt.is('ios');
-    }
 
-    stopListening() {
-        this.speechRecognition.stopListening().then(() => {
-            this.isRecording = false;
+    async isSpeechSupported(): Promise<boolean> {
+        let isAvailable = await this.speech.isRecognitionAvailable();
+        console.log(isAvailable);
+
+
+        let alert = this.alertCtrl.create({
+            title: 'Output',
+            message: "" + isAvailable
         });
+        alert.present();
+
+        return isAvailable;
     }
 
-    getPermission() {
-        this.speechRecognition.hasPermission()
-            .then((hasPermission: boolean) => {
-                if (!hasPermission) {
-                    this.speechRecognition.requestPermission();
-                }
+    async getPermission(): Promise<void> {
+        try {
+            let permission = await this.speech.requestPermission();
+            console.log(permission);
+
+            let alert = this.alertCtrl.create({
+                title: 'Output',
+                message: "" + permission
             });
+            alert.present();
+
+            return permission;
+        }
+        catch (e) {
+            console.error(e);
+        }
     }
 
-    startListening() {
-        let options = {
+    async hasPermission(): Promise<boolean> {
+        try {
+            let permission = await this.speech.hasPermission();
+            console.log(permission);
+
+            let alert = this.alertCtrl.create({
+                title: 'Output',
+                message: "" + permission
+            });
+            alert.present();
+
+            return permission;
+        }
+        catch (e) {
+            console.error(e);
+        }
+    }
+
+    async getSupportedLanguages(): Promise<Array<string>> {
+        try {
+            let languages = await this.speech.getSupportedLanguages();
+            console.log(languages);
+
+            let alert = this.alertCtrl.create({
+                title: 'Output',
+                message: "" + languages
+            });
+            alert.present();
+
+            return languages;
+        }
+        catch (e) {
+            console.error(e);
+        }
+    }
+
+    listenForSpeech(): void {
+
+        this.androidOptions = {
+            prompt: 'Speak into your phone!'
+        }
+
+        this.iosOptions = {
             language: 'en-US'
         }
-        this.speechRecognition.startListening().subscribe(matches => {
-            this.matches = matches;
-            this.cd.detectChanges();
-        });
-        this.isRecording = true;
-    }
 
+        if (this.platform.is('android')) {
+            this.speech.startListening(this.androidOptions).subscribe(data => this.speechList = data, error => console.log(error));
+        }
+        else if (this.platform.is('ios')) {
+            this.speech.startListening(this.iosOptions).subscribe(data => this.speechList = data, error => console.log(error));
+        }
+    }
 }
